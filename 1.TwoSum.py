64f12e19a86334fe4672d6039fee252e565a7f09
sharddawg <78741839+sharddawg@users.noreply.github.com>
2021-10-04 23:13:53 +0530

    Create 1.TwoSum.py

---
+++ 1.TwoSum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dictionary = {}
+        for i, num in enumerate(nums):
+            rem = target-num
+            if rem in dictionary:
+                return [dictionary[rem], i]
+            dictionary[num] = i
+        return None
